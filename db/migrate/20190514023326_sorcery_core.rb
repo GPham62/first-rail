93216c3188492b0646fd647c69d7b7fd3fe47beb
GPham62 <ptuananh196@gmail.com>
2019-05-14 10:42:49 +0700

    fix pwd

--- db/migrate/20190514023326_sorcery_core.rb
+++ db/migrate/20190514023326_sorcery_core.rb
@@ -2,11 +2,10 @@ class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :authors do |t|
       t.string :email,            :null => false
-      t.string :crypted_password, :null => false
+      t.string :crypted_password,
       t.string :salt,             
       t.string :username,         
-
-      t.timestamps                :null => false
+      t.timestamps              
     end
 
     add_index :authors, :email, unique: true
